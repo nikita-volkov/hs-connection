649a198534c4950d2452bb68c0b1b7539462e1a6
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-08-19 11:13:37 +0300

    Add multiple sizes

--- debug/Main.hs
+++ debug/Main.hs
@@ -6,27 +6,33 @@ import Prelude
 import qualified Data.ByteString.Char8 as ByteString
 import Network.Connection
 import System.Exit
+import System.Timeout
 
 main = do
-  ctx <- initConnectionContext
-  con <-
-    connectTo ctx $
-      ConnectionParams
-        { connectionHostname = "apigw.yandexcloud.net",
-          connectionPort = 443,
-          connectionUseSecure = Just $ def,
-          connectionUseSocks = Nothing
-        }
-  traceM "+ send"
-  connectionPut con $ ByteString.replicate 30000 'z'
-  traceM "- send"
-  traceM "+ recv"
-  avail <- connectionWaitForInput con 15000
-  if avail
-    then do
-      r <- connectionGetChunk con
-      traceM "- recv"
-      putStrLn "Printing response:"
-      ByteString.putStrLn r
-      connectionClose con
-    else die "Timed out"
+  test 1000
+  test 30000
+
+test requestSize = do
+    putStrLn $ "Testing with request of size " <> show requestSize
+    ctx <- initConnectionContext
+    con <-
+      connectTo ctx $
+        ConnectionParams
+          { connectionHostname = "apigw.yandexcloud.net",
+            connectionPort = 443,
+            connectionUseSecure = Just $ def,
+            connectionUseSocks = Nothing
+          }
+    connectionPut con $ ByteString.replicate requestSize 'z'
+
+    res <- timeoutInSeconds 15 $ connectionGetChunk con
+    res <- case res of
+      Nothing -> die "Timeout"
+      Just res -> return res
+
+    putStrLn "Printing response:"
+    ByteString.putStrLn res
+
+timeoutInSeconds seconds =
+  timeout (seconds * 1000000)
+
